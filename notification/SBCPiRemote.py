ddd7d80140f4677a0d1a91a0abac506b372dfeb1
Matt Willoughby <m.aar.dub@gmail.com>
2017-04-08 18:25:04 -0500

    Working notification script using json serialized data file

--- notification/SBCPiRemote.py
+++ notification/SBCPiRemote.py
@@ -1,7 +1,11 @@
+import sys
 import webiopi
 import RPi.GPIO as RPIGPIO
-import httplib
 import json
+if sys.version_info[0] < 3:
+	import httplib
+else:
+	import http.client
 
 # Enable debug output
 webiopi.setDebug()
@@ -23,58 +27,77 @@ for item in contents.split("\n"):
 # import webiopi's GPIO library
 webiopiGPIO = webiopi.GPIO
 
-def postToAppEngine(pin):
-	URL = "piremote-160105.appspot.com/apns/"
-	# is there supposed to be a : at the end?
-	# URL = "piremote-160105.appspot.com/apns/:"
-	httpServ = httplib.HTTPSConnection(URL, 443)
-	httpServ.connect()
+# store python version
+pyVersion = sys.version_info[0]
 
-	jsonPin = json.dumps({'pin': pin})
-	httpServ.request('POST', '/%s' % UID, jsonPin)
-
-	response = httpServ.getresponse()
-	if response.status == httplib.OK:
+def postToAppEngine(message):
+	URL = "piremote-160105.appspot.com/apns"
+	
+	headers = {"Content-type": "application/json", "Accept": "text/plain"}
+	
+	if pyVersion < 3:
+		conn = httplib.HTTPSConnection(URL, 443)
+	else:
+		conn = http.client.HTTPSConnection(URL, 443)
+		
+	conn.request('POST', '/%s' % UID, message, headers)
+	response = conn.getresponse()
+	if response.status == 200:
 		webiopi.debug("Notification sent to App Engine")
 	else:
 		webiopi.debug("Notification failed to send")
+	conn.close()
 
-	httpServ.close()
-	
 ###################### Define the callback functions ##################
-
 def callback(gpio):
-	if webiopiGPIO.getFunction(gpio) == webiopiGPIO.IN:
-		postToAppEngine(gpio)
-
+	# if webiopiGPIO.getFunction(gpio) == webiopiGPIO.IN:
+	jsonPin = json.dumps({'pin': gpio})
+	# postToAppEngine(jsonPin)
+	webiopi.debug("Notification sent to App Engine")
+	
+	
 # setup() is called by WebIOPi when it starts up
 def setup():
-	#set the RPi GPIO mode to BCM numbering
-	RPIGPIO.setmode(RPIGPIO.BCM)
-	
-	# Set all GPIO pins as inputs with the RPi.GPIO library.
-	# This is hacky because webiopi already controls what the pin types are,
-	# but it's necessary in order to set the RPi.GPIO event listeners
-	RPIGPIO.setup(chan_list, RPIGPIO.IN)
-	
-	### TODO: Set pins to previous configuration  ##
-	# If we're able to store the prev config in the iOS app,
-	# maybe we can send the app a notification at this point
-	# which would trigger the configuration push to the pi
-	
+
 	# Send iOS app a notification that the pi just restarted.
-	# This could be the notification which triggers the pin
-	# configuration mentioned above.
-	
+	# restartMessage = json.dumps({'message': 'pi restarted'})
+	# postToAppEngine(restartMessage)
+	webiopi.debug("Notification - Webiopi restarted")
 	
 	# Turn off RPi's warnings
 	RPIGPIO.setwarnings(False)
 	
-	# Add event listener for each pin
+	#set the RPi GPIO mode to BCM numbering
+	RPIGPIO.setmode(RPIGPIO.BCM)
+	
+	# Use the RPi.GPIO library to setup each pin as its current state.
+	# This is hacky because webiopi already controls what the pin types are,
+	# but it's necessary in order to set the RPi.GPIO event listeners
+	# RPIGPIO.setup(chan_list, RPIGPIO.IN)
 	for gpio in chan_list:
+		RPIGPIO.setup(gpio, RPIGPIO.IN)
+		# Add event listener for each pin
 		RPIGPIO.add_event_detect(gpio, RPIGPIO.BOTH)
 		RPIGPIO.add_event_callback(gpio, callback)
 	
+	# Set pins to previous configuration
+	pinConfig = open('/home/pi/webiopi/scripts/gpioConfig','r')
+	values = json.load(pinConfig)
+	for gpio, funct in values.items():
+		webiopiGPIO.setFunction(int(gpio), int(funct))
+	pinConfig.close()
+
+def loop():
+	values = {}
+	for gpio in chan_list:
+		func = webiopiGPIO.getFunction(gpio)
+		values[str(gpio)] = func
+	pinConfig = open('/home/pi/webiopi/scripts/gpioConfig','w')
+	json.dump(values, pinConfig)
+	pinConfig.close()
+	
+	webiopi.sleep(1)
+
 # destroy() is called by WebIOPi when it shuts down
 def destroy():
 	# Remove event listeners for each pin
