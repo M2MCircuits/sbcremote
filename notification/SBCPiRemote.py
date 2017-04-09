bc13102517390a18428f80a0856c5bd501765c74
Matt Willoughby <m.aar.dub@gmail.com>
2017-04-09 12:05:19 -0500

    Completed working notification script with sqlite3 database

--- notification/SBCPiRemote.py
+++ notification/SBCPiRemote.py
@@ -2,6 +2,8 @@ import sys
 import webiopi
 import RPi.GPIO as RPIGPIO
 import json
+import sqlite3
+import os
 if sys.version_info[0] < 3:
 	import httplib
 else:
@@ -29,6 +31,8 @@ webiopiGPIO = webiopi.GPIO
 
 # store python version
 pyVersion = sys.version_info[0]
+
+pinConfigDB = "/etc/webiopi/gpio_config.db"
 
 def postToAppEngine(message):
 	URL = "piremote-160105.appspot.com/apns"
@@ -74,6 +78,7 @@ def setup():
 	# This is hacky because webiopi already controls what the pin types are,
 	# but it's necessary in order to set the RPi.GPIO event listeners
 	# RPIGPIO.setup(chan_list, RPIGPIO.IN)
+	webiopi.debug("Setting all pins as input")
 	for gpio in chan_list:
 		RPIGPIO.setup(gpio, RPIGPIO.IN)
 		# Add event listener for each pin
@@ -81,25 +86,63 @@ def setup():
 		RPIGPIO.add_event_callback(gpio, callback)
 	
 	# Set pins to previous configuration
-	pinConfig = open('/home/pi/webiopi/scripts/gpioConfig','r')
-	values = json.load(pinConfig)
-	for gpio, funct in values.items():
-		webiopiGPIO.setFunction(int(gpio), int(funct))
-	pinConfig.close()
+	sqlconn = sqlite3.connect(pinConfigDB)
+	c = sqlconn.cursor()
+	try:
+		webiopi.debug("Attempting to access database")
+		c.execute('SELECT pin, function FROM pins')
+		webiopi.debug("fetching contents of pins table")
+		pinList = c.fetchall()
+		webiopi.debug("Checking if it was empty")
+		if not pinList:
+			webiopi.debug("the table was empty")
+			raise sqlite3.OperationalError
+		webiopi.debug("setting the pin functions")
+		for pin in pinList:
+			webiopiGPIO.setFunction(pin[0], pin[1])
+	except sqlite3.OperationalError:
+		# Build new table
+		webiopi.debug("Attempting to create new pins table")
+		try:
+			c.execute('CREATE TABLE pins (pin integer, function integer)')
+			webiopi.debug("Attempting to create new pins table")
+		except sqlite3.OperationalError:
+			webiopi.debug("pins table created")
+			pass
+		pinList = []
+		webiopi.debug("filling pins table with default values")
+		for gpio in chan_list:
+			newFunct = (gpio, 0)
+			pinList.append(newFunct)
+		c.executemany('INSERT INTO pins VALUES (?,?)', pinList)
+		webiopi.debug("Committing new database changes")
+		sqlconn.commit()
+	except:
+		error = "Unable to access the pin database: " + sys.exc_info()[0]
+		webiopi.debug(error)
+	webiopi.debug("Closing database")
+	sqlconn.close()
 
 def loop():
-	values = {}
+	pinList = []
 	for gpio in chan_list:
 		func = webiopiGPIO.getFunction(gpio)
-		values[str(gpio)] = func
-	pinConfig = open('/home/pi/webiopi/scripts/gpioConfig','w')
-	json.dump(values, pinConfig)
-	pinConfig.close()
-	
+		newFunct = (func, gpio)
+		pinList.append(newFunct)
+	sqlconn = sqlite3.connect(pinConfigDB)
+	c = sqlconn.cursor()
+	try:
+		c.executemany('UPDATE pins SET function=? WHERE pin=?', pinList)
+		sqlconn.commit()
+	except:
+		error = "Unable to update the pin database: " + sys.exc_info()[0]
+		webiopi.debug(error)
+	sqlconn.close()
 	webiopi.sleep(1)
 
 # destroy() is called by WebIOPi when it shuts down
 def destroy():
 	# Remove event listeners for each pin
+	webiopi.debug("Removing pin listeners")
 	for gpio in chan_list:
 		RPIGPIO.remove_event_detect(gpio)
