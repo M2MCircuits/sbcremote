462f78b9671aefe6d2a5acdb63ac3863aafe5705
Victor Anyirah <victoranyirah@yahoo.com>
2017-04-14 01:26:45 -0500

    - Fizes weird segue in storyboard

--- AppEngineServer/server/main.py
+++ AppEngineServer/server/main.py
@@ -22,7 +22,7 @@ from google.appengine.ext import ndb
 
 #Configs mapped to the largest possible pin value (28)
 default_config = [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
-valid pins = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,18,23,24,25,8,7,12,16,20,21]
+valid_pins = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,18,23,24,25,8,7,12,16,20,21]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
