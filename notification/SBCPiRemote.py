b15a2b9e0c9958131dab157275ed311f2c40cd7a
matt <m.aar.dub@gmail.com>
2017-04-16 17:51:02 -0500

    Utilize 'Requests' for http requests

--- notification/SBCPiRemote.py
+++ notification/SBCPiRemote.py
@@ -4,10 +4,7 @@ import RPi.GPIO as RPIGPIO
 import json
 import sqlite3
 import os
-if sys.version_info[0] < 3:
-	import httplib
-else:
-	import http.client
+import requests
 
 # Enable debug output
 webiopi.setDebug()
@@ -25,42 +22,29 @@ UID = "filenotfound"
 for item in contents.split("\n"):
 	if "UID" in item:
 		UID = item.split(" ")[1]
-		
+
+# App Engine URL
+url = "https://piremote-160105.appspot.com/apns/" + UID
+
 # Import webiopi's GPIO library
 webiopiGPIO = webiopi.GPIO
-
-# Store python version
-pyVersion = sys.version_info[0]
 
 # Define the gpio pin configuration database path
 pinConfigDB = "/etc/webiopi/gpio_config.db"
 
 # This function sends a json encoded message to the App Engine
-def postToAppEngine(message):
-	# App Engine URL
-	URL = "piremote-160105.appspot.com/apns"
-	
-	# Set content type as json in header
-	headers = {"Content-type": "application/json", "Accept": "text/plain"}
-	
-	# Determine which http library should be used
-	if pyVersion < 3:
-		# Establish python2 http connection
-		conn = httplib.HTTPSConnection(URL, 443)
-	else:
-		# Establish python3 http connection
-		conn = http.client.HTTPSConnection(URL, 443)
+def postToAppEngine(jsonMessage):
+
 	# Send the http POST to App Engine
-	conn.request('POST', '/%s' % UID, message, headers)
-	# Collect the response
-	response = conn.getresponse()
+	response = requests.post(url, data=jsonMessage)
+
 	# Check for errors
-	if response.status == 200:
+	if response.status_code == 200:
 		webiopi.debug("Notification sent to App Engine")
 	else:
-		webiopi.debug("Notification failed to send")
-	# Close the http connection
-	conn.close()
+		# errorMessage = jsonMessage + " -- Notification failed to send: " + str(response.status_code) + " - " + response.text
+		errorMessage = jsonMessage + " -- Notification failed to send: Status Code " + str(response.status_code)
+		webiopi.debug(errorMessage)
 
 ###################### Define the callback function ##################
 def callback(gpio):
@@ -68,7 +52,7 @@ def callback(gpio):
 	# if webiopiGPIO.getFunction(gpio) == webiopiGPIO.IN:
 	# jsonify the pin
 	# message=1 (pin change), funct(Function): 0=input 1=output, val(Value): 0=off(low) 1=on(high)
-	jsonPin = json.dumps({'message' : 1, 'pin': gpio, 'funct' : webiopiGPIO.getFunction(gpio), 'val' : webiopiGPIO.digitalRead(gpio)})
+	jsonPin = json.dumps({'message' : 1, 'pin': gpio, 'funct' : webiopiGPIO.getFunction(gpio), 'val' : int(webiopiGPIO.digitalRead(gpio))})
 	# Post the pin to App Engine
 	postToAppEngine(jsonPin)
 	
@@ -92,40 +76,42 @@ def setup():
 	# but it's necessary in order to set the RPi.GPIO event listeners
 	# RPIGPIO.setup(chan_list, RPIGPIO.IN)
 	webiopi.debug("Setting all pins as input")
+	webiopi.debug("Adding event listeners for each pin")
 	for gpio in chan_list:
 		RPIGPIO.setup(gpio, RPIGPIO.IN)
 		# Add event listener for each pin
 		RPIGPIO.add_event_detect(gpio, RPIGPIO.BOTH)
-		RPIGPIO.add_event_callback(gpio, callback)
-	
+
 	# Set pins to previous configuration
 	sqlconn = sqlite3.connect(pinConfigDB)
 	c = sqlconn.cursor()
 	try:
 		# Fetch pin configuration from DB
 		c.execute('SELECT pin, function FROM pins')
-		webiopi.debug("fetching contents of pins table")
+		webiopi.debug("Fetching contents of -pins- table")
 		pinList = c.fetchall()
-		webiopi.debug("Checking if it was empty")
+		webiopi.debug("Checking if -pins- table is empty")
 		# If the list is empty, raise exception
 		if not pinList:
-			webiopi.debug("the table was empty")
+			webiopi.debug("The -pins- table was empty")
 			raise sqlite3.OperationalError
-		webiopi.debug("setting the pin functions")
+		webiopi.debug("Setting the pin functions")
 		# Set each pin's function according to the database
 		for pin in pinList:
 			webiopiGPIO.setFunction(pin[0], pin[1])
+			if pin[1] == webiopiGPIO.OUT:
+				webiopiGPIO.digitalWrite(pin[0], webiopiGPIO.LOW)
 	except sqlite3.OperationalError:
 		# Build new table
-		webiopi.debug("Create new pins table")
+		webiopi.debug("Create new -pins- table")
 		try:
 			c.execute('CREATE TABLE pins (pin integer, function integer)')
 		except sqlite3.OperationalError:
-			webiopi.debug("pins table already exists")
+			webiopi.debug("-pins- table already exists")
 			pass
 		pinList = []
 		# Set the table with default values (input)
-		webiopi.debug("filling pins table with default values")
+		webiopi.debug("Filling -pins- table with default values")
 		for gpio in chan_list:
 			newFunct = (gpio, 0)
 			pinList.append(newFunct)
@@ -135,10 +121,14 @@ def setup():
 		# Commit the database changes
 		sqlconn.commit()
 	except:
-		error = "Unable to access the pin database: " + sys.exc_info()[0]
+		error = "Unable to access the pin database: " + str(sys.exc_info()[0])
 		webiopi.debug(error)
 	webiopi.debug("Closing database")
 	sqlconn.close()
+	webiopi.debug("Adding event callbacks for each pin")
+	# Add event callbacks
+	for gpio in chan_list:
+		RPIGPIO.add_event_callback(gpio, callback)
 
 # loop() is periodically called by webiopi as it runs
 def loop():
@@ -161,8 +151,8 @@ def loop():
 	webiopi.sleep(1)
 
 # destroy() is called by WebIOPi when it shuts down
-def destroy():
-	# Remove event listeners for each pin
-	webiopi.debug("Removing pin listeners")
-	for gpio in chan_list:
-		RPIGPIO.remove_event_detect(gpio)
+# def destroy():
+	# # Remove event listeners for each pin
+	# webiopi.debug("Removing pin listeners")
+	# for gpio in chan_list:
+		# RPIGPIO.remove_event_detect(gpio)
